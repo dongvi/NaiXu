2328e6f7776d513fa268ba0ebf9efb38c773a892
ViDong <dongvi280201@gmail.com>
2022-04-28 17:48:47 +0700

    [Update] Animation to logo, change colors background, splash full screen.

--- app/src/main/java/com/projectbase/base/ultils/extentions/ViewExtension.kt
+++ app/src/main/java/com/projectbase/base/ultils/extentions/ViewExtension.kt
@@ -1,6 +1,8 @@
 package com.projectbase.base.ultils.extentions
 
+import android.content.Context
 import android.view.View
+import android.view.animation.AnimationUtils
 
 fun View.visible() {
     if (visibility != View.VISIBLE) {
@@ -18,4 +20,9 @@ fun View.invisible() {
     if (visibility != View.INVISIBLE) {
         visibility = View.INVISIBLE
     }
+}
+
+fun View.setAnim(context: Context?, anim: Int, duration: Long) {
+    this.animation = AnimationUtils.loadAnimation(context, anim)
+    this.animation.duration = duration
 }
